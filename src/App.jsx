e3d0639362d71e750c7d951768e8f0efbec8a5ef
Palathip <windeesel365@gmail.com>
2024-02-04 21:12:48 +0700

    feat(react_state):task

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import React, { useState } from 'react';
 import "./App.css";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+
+  const handleButtonClick = (newGreeting) => {
+    setGreeting(newGreeting);
+  };
+    
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => handleButtonClick("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => handleButtonClick("Hi!")}>Hi!</button>
+        <button onClick={() => handleButtonClick("你好!")}>你好!</button>        
+
       </div>
     </div>
   );
